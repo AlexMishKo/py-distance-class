51aff922858032ebcf177fd63be6c7068a00fab5
Oleksii Mishchenko <alex3hard@gmail.com>
2025-10-29 14:17:34 +0200

    rewrighted some type annotations

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from typing import Union
 
 
@@ -12,7 +13,7 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def _get_km_value(self, other: Union[int, float, "Distance"])\
+    def _get_km_value(self, other: Union[int, float, Distance])\
             -> Union[int, float, type(NotImplemented)]:
         if isinstance(other, Distance):
             return other.km
@@ -21,67 +22,67 @@ class Distance:
         else:
             return NotImplemented
 
-    def __add__(self, other: Union[int, float, "Distance"])\
-            -> Union["Distance", type(NotImplemented)]:
+    def __add__(self, other: Union[int, float, Distance])\
+            -> Union[Distance, type(NotImplemented)]:
         other_km = self._get_km_value(other)
         if other_km is NotImplemented:
             return NotImplemented
         return Distance(self.km + other_km)
 
-    def __radd__(self, other: Union[int, float, "Distance"]) -> "Distance":
+    def __radd__(self, other: Union[int, float, Distance]) -> Distance:
         return self.__add__(other)
 
-    def __iadd__(self, other: Union[int, float, "Distance"]) \
-            -> Union["Distance", type(NotImplemented)]:
+    def __iadd__(self, other: Union[int, float, Distance]) \
+            -> Union[Distance, type(NotImplemented)]:
         other_km = self._get_km_value(other)
         if other_km is NotImplemented:
             return NotImplemented
         self.km += other_km
         return self
 
-    def __mul__(self, other: Union[int, float, "Distance"]) \
-            -> Union["Distance", type(NotImplemented)]:
+    def __mul__(self, other: Union[int, float]) \
+            -> Union[Distance, type(NotImplemented)]:
         if not isinstance(other, (int, float)):
             return NotImplemented
         return Distance(self.km * other)
 
-    def __rmul__(self, other: Union[int, float, "Distance"]) -> "Distance":
+    def __rmul__(self, other: Union[int, float]) -> Distance:
         return self.__mul__(other)
 
-    def __truediv__(self, other: Union[int, float, "Distance"])\
-            -> Union["Distance", type(ZeroDivisionError)]:
+    def __truediv__(self, other: Union[int, float])\
+            -> Union[Distance, float, type(NotImplemented)]:
         if not isinstance(other, (int, float)):
             return NotImplemented
         if other == 0:
-            return ZeroDivisionError
+            raise ZeroDivisionError("Cannot divide by zero")
         result = round(self.km / other, 2)
         return Distance(result)
 
-    def __lt__(self, other: Union[int, float, "Distance"]) -> bool:
+    def __lt__(self, other: Union[int, float, Distance]) -> bool:
         other_km = self._get_km_value(other)
         if other_km is NotImplemented:
             return NotImplemented
         return self.km < other_km
 
-    def __gt__(self, other: Union[int, float, "Distance"]) -> bool:
+    def __gt__(self, other: Union[int, float, Distance]) -> bool:
         other_km = self._get_km_value(other)
         if other_km is NotImplemented:
             return NotImplemented
         return self.km > other_km
 
-    def __eq__(self, other: Union[int, float, "Distance"]) -> bool:
+    def __eq__(self, other: Union[int, float, Distance]) -> bool:
         other_km = self._get_km_value(other)
         if other_km is NotImplemented:
             return NotImplemented
         return self.km == other_km
 
-    def __le__(self, other: Union[int, float, "Distance"]) -> bool:
+    def __le__(self, other: Union[int, float, Distance]) -> bool:
         other_km = self._get_km_value(other)
         if other_km is NotImplemented:
             return NotImplemented
         return self.km <= other_km
 
-    def __ge__(self, other: Union[int, float, "Distance"]) -> bool:
+    def __ge__(self, other: Union[int, float, Distance]) -> bool:
         other_km = self._get_km_value(other)
         if other_km is NotImplemented:
             return NotImplemented
